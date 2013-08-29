c13e97c33da34275ac8ebb718196c4d17d3cea76
Michael Liu <michael.liu95@gmail.com>
2013-08-29 18:41:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hbook::Application.config.secret_key_base = '9897589241ecf61158397b74e00f00664fa2a9b685d55d64fb438b2b46c88096861a1ffc3c33c9bcf290b0754f5238c0c0c8a0c47250430fe0f6682cafdb3238'
