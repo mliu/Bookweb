bb583415f3c2403902bd9a2361f9f48d129a7c15
mliu95 <michael.liu95@gmail.com>
2013-09-13 00:59:30 -0400

    end commit.

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,9 +1,9 @@
 module BooksHelper
   def field_class(resource, field_name)
     if resource.errors[field_name].present?
-      return "error listalign".html_safe
+      return "error".html_safe
     else
-      return "noerror listalign".html_safe
+      return "noerror".html_safe
     end
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -10,6 +10,7 @@ class Book < ActiveRecord::Base
   # Relationships
   belongs_to :school, class_name: "School"
   belongs_to :user, class_name: "User"
+  
 
   def self.search(department, course_num)
     Book.where("department = ? AND course_num = ?", department, course_num.to_s)
