2eab6b9da9c6a902fedd4aa1a197bb79ae1ff997
Daniel Golman <dgolman@vt.edu>
2013-11-27 15:36:28 -0500

    Added migration db file and fixed centering of home page

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130902023737) do
+ActiveRecord::Schema.define(version: 20131127203117) do
 
   create_table "books", force: true do |t|
-    t.integer  "school_id"
+    t.string   "school_id"
+    t.string   "email"
     t.string   "department"
     t.integer  "course_num"
     t.string   "title"
-    t.string   "isbn"
-    t.string   "author"
+    t.integer  "isbn"
     t.string   "condition"
     t.decimal  "asking_price"
-    t.string   "trade",        default: ""
-    t.string   "details",      default: ""
-    t.string   "contact"
-    t.datetime "expiration",   default: '2013-09-29 03:59:59'
+    t.datetime "expiration",   default: '2013-12-12 04:59:59'
     t.datetime "created_at"
     t.datetime "updated_at"
   end
